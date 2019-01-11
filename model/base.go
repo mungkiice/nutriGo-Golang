d03d99dc031d13fe8f214e14fdf3cde13bd57ad6
Muhammad Iqbal Kurniawan <m.kurniawanibal@gmail.com>
2019-01-11 18:53:01 +0700

    delete deleted_at column

--- model/base.go
+++ model/base.go
@@ -6,5 +6,5 @@ type Model struct {
 	ID        uint       `gorm:"primary_key" json:"id"`
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt time.Time  `json:"updated_at"`
-	DeletedAt *time.Time `json:"deleted_at,omitempty"`
+	//DeletedAt *time.Time `json:"deleted_at,omitempty"`
 }
--- model/user.go
+++ model/user.go
@@ -1,5 +1,7 @@
 package model
 
+import "golang.org/x/crypto/bcrypt"
+
 type User struct{
 	Model
 	Nama		string		`gorm:"type:varchar(191);not_null"`
@@ -11,4 +13,15 @@ type User struct{
 	Usia		int			`gorm:"type:int(11)"`
 	IsAdmin		bool		`gorm:"type:tinyint(1);default:0"`
 	History		[]History
+}
+
+func (User) TableName() string {
+	return "user"
+}
+
+func (u *User) VerifyPassword(pass string) error {
+	if err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(pass)); err != nil {
+		return err
+	}
+	return nil
 }
